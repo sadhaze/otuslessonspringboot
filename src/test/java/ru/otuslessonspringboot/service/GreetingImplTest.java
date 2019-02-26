d040cbf0a4359c34a7bc9249d5f71801a2325f77
anton <psycore@mail.ru>
2019-02-26 17:22:19 +0300

    1) переработан механизм локализации 2) добработаны тесты

--- src/test/java/ru/otuslessonspringboot/service/GreetingImplTest.java
+++ src/test/java/ru/otuslessonspringboot/service/GreetingImplTest.java
@@ -5,33 +5,34 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.MessageSource;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import ru.otuslessonspringboot.config.YamlProps;
 
 import java.io.ByteArrayInputStream;
+import java.util.Locale;
 
 @SpringBootTest
 @DisplayName("Тест приветствия")
 class GreetingImplTest {
-    @Autowired
-    private MessageSource messageSource;
+    @SpyBean
+    private YamlProps props;
 
     @Autowired
-    private Greeting greetingServiceEn;
-
-    @Autowired
-    private Greeting greetingServiceRu;
+    private Greeting greetingService;
 
     @Test
     @DisplayName("Тест приветствия english")
     void greetingTest_En(){
+        props.setLocale(new Locale("en", "EN"));
         System.setIn(new ByteArrayInputStream("Яимя\nЯфамилия\n".getBytes()));
-        Assertions.assertEquals("Hello, Яфамилия Яимя!", greetingServiceEn.getGreeting());
+        Assertions.assertEquals("Hello, Яфамилия Яимя!", greetingService.getGreeting());
     }
 
     @Test
     @DisplayName("Тест приветствия русский")
     void greetingTest_Ru(){
+        props.setLocale(new Locale("ru", "RU"));
         System.setIn(new ByteArrayInputStream("Яимя\nЯфамилия\n".getBytes()));
-        Assertions.assertEquals("пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ, Яфамилия Яимя!", greetingServiceRu.getGreeting());
+        Assertions.assertEquals("пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ, Яфамилия Яимя!", greetingService.getGreeting());
     }
 }
